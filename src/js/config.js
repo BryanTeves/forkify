e3d03acb3daa7ad522f0ad068be5d8a5d2904b7e
BryanTeves <bryanteves2013@hotmail.com>
2022-07-11 12:54:17 -0300

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = `https://forkify-api.herokuapp.com/api/v2/recipes/`;
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = "df9a0997-8f09-467f-b0a6-fcec76069134";
+export const MODAL_CLOSE_SEC = 2.5;
