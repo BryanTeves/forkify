aad79c411395542801d8d02142cd7b7350b6e131
BryanTeves <bryanteves2013@hotmail.com>
2022-07-11 13:07:14 -0300

    Added new welcome feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -127,6 +127,10 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
+const newFeature = function () {
+  console.log("Welcome to the application!");
+};
+
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -135,6 +139,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearhResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
-  console.log("Welcome!");
+  newFeature();
 };
 init();
